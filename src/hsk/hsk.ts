9ab747ac72c3cee72e3a98d4d6aaa46cbcc99c3f
Sebastian Göb <sebastiancgoeb@gmail.com>
2023-12-10 20:30:03 +0100

    remove console log

--- src/hsk/hsk.ts
+++ src/hsk/hsk.ts
@@ -18,7 +18,6 @@ export enum Revision {
 
 export async function getWordsTsv(revision: Revision): Promise<HskWord[]> {
   const prefix = revision === Revision.NEW ? "new" : "old";
-  console.log(import.meta.url);
   const res = await fetch(`${prefix}_hsk.tsv`);
   return wordsTsvToJson(await res.text());
 }

abc732bf19981e1021ad98c465becc66944f9824
Sebastian Göb <sebastiancgoeb@gmail.com>
2023-12-16 20:49:08 +0100

    fix: anki interval script should output to /public

--- scripts/extract_anki_collection.sh
+++ scripts/extract_anki_collection.sh
@@ -3,4 +3,4 @@
 sqlite3 -readonly \
   "$HOME/Library/Application Support/Anki2/User 1/collection.anki2" \
   <./scripts/hsk_notes.sql \
-  >./src/assets/new_hsk_anki_notes.tsv
+  >./public/new_hsk_anki_notes.tsv

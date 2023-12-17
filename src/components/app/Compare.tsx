179cb7848a27a1648411eba16a19350b78f2249b
Sebastian Göb <sebastiancgoeb@gmail.com>
2023-12-17 07:21:07 +0100

    create comparison grid

--- src/components/app/Compare.tsx
+++ src/components/app/Compare.tsx
@@ -1,22 +1,65 @@
-import { For, createSignal } from "solid-js";
+import { intersectionBy } from "lodash";
+import { For, createMemo, createResource, createSignal } from "solid-js";
+import {
+  HskLevel,
+  HskWord,
+  Revision,
+  getWordsTsv,
+  groupWordsByLevel,
+} from "../../hsk/hsk";
 
 enum CompareOption {
   HSK = "HSK",
   活着 = "活着",
+  Subtlex = "Subtlex",
 }
 
 const optionLevels = new Map<CompareOption, number[]>();
 optionLevels.set(CompareOption.HSK, [1, 2, 3, 4, 5, 6]);
-optionLevels.set(CompareOption.活着, [200, 500, 2000, 5000]);
+optionLevels.set(CompareOption.活着, [200, 500, 2000, 5000, Infinity]);
+optionLevels.set(CompareOption.Subtlex, [200, 500, 2000, 5000, Infinity]);
+
+function calculatePairings(levelsA: HskLevel[], levelsB: HskLevel[]) {
+  const pairings = new Map<string, HskWord[]>();
+
+  // init empty lists
+  levelsA.forEach((a) => {
+    levelsB.forEach((b) => {
+      const commonWords = intersectionBy(
+        a.words,
+        b.words,
+        (hskWord) => hskWord.chinese,
+      );
+      pairings.set(a.level + "-" + b.level, commonWords);
+    });
+  });
+
+  return pairings;
+}
 
 function Compare() {
-  const options = [CompareOption.HSK, CompareOption.活着];
+  const options = [
+    CompareOption.HSK,
+    CompareOption.活着,
+    CompareOption.Subtlex,
+  ];
+
+  const [words] = createResource(Revision.NEW, getWordsTsv, {
+    initialValue: [],
+  });
 
   const [optionA, setOptionA] = createSignal<CompareOption>(CompareOption.HSK);
-  const [optionB, setOptionB] = createSignal<CompareOption>(CompareOption.活着);
+  const [optionB, setOptionB] = createSignal<CompareOption>(CompareOption.HSK);
+
+  const pairings = createMemo(() => {
+    const wordsA = groupWordsByLevel(words());
+    const wordsB = groupWordsByLevel(words());
+    return calculatePairings(wordsA, wordsB);
+  });
 
   return (
-    <div class="px-16 py-4">
+    <div class="flex w-auto flex-col items-center px-16 py-4">
+      {/* selector */}
       <div class="flex items-center gap-4">
         Compare
         <select
@@ -35,7 +78,7 @@ function Compare() {
       </div>
 
       {/* table */}
-      <table>
+      <table class="mt-4  border-spacing-2 border border-slate-500">
         <thead>
           <tr>
             <th></th>
@@ -50,7 +93,11 @@ function Compare() {
               <tr>
                 <th>{levelA}</th>
                 <For each={optionLevels.get(optionB())}>
-                  {(_levelB) => <td>-</td>}
+                  {(levelB) => (
+                    <td class="border border-slate-700 px-4 py-1">
+                      {pairings().get(levelA + "-" + levelB)?.length}
+                    </td>
+                  )}
                 </For>
               </tr>
             )}
